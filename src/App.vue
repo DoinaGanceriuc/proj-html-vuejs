767f1f62006ddac4da4d8b2040da47ed8ec7d898
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-25 15:46:39 +0100

    install bootstrap and font-family

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,7 @@ export default {
 </script>
 
 <style lang="scss">
+@import '../node_modules/bootstrap/scss/bootstrap.scss';
 @import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 @import './assets/scss/common.scss';
 </style>
