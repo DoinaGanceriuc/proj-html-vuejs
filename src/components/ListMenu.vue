699904751bc6f60201921a00ff4b55b29632edb2
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-25 11:42:17 +0100

    complete details site header

---
+++ src/components/ListMenu.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul>
+      <li class="active">HOME</li>
+       <li v-for="list in lists" :key="list">{{list.toUpperCase()}}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    lists: Array
+  }
+}
+</script>
+
+<style lang="scss">
+ul {
+  display: flex;
+  list-style: none;
+  li {
+    padding: 0 1rem;
+    font-size: 0.5rem;
+    color: #74559F;
+  }
+  .active {
+      color: #74559F;
+      &::after {
+        content: "";
+        display: block;
+        width: 30px;
+        height: 2px;
+        background-color: #74559F;
+        margin: auto;
+        margin-top: 5px;
+      }
+    }
+}
+</style>
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,10 +1,45 @@
 <template>
   <div>
+    <header id="site_header">
+      <div class="container">
+         <div class="logo">
+        <img src="../assets/img/avada-bakery-logo.png" alt="logo baker purple" width="125"/>
+      </div>
+      <!-- /.logo -->
+         <ListMenu :lists="lists"/>
+      </div>
+    </header>
+    <!-- /#site_header -->
   </div>
 </template>
 
 <script>
+import ListMenu from '../components/ListMenu.vue'
+export default {
+  components: {
+    ListMenu
+  },
+  data () {
+    return {
+      lists: ['shop', 'about', 'gallery', 'locations', 'journal', 'contact', 'my account', 'carrello']
+
+    }
+  }
+}
+
 </script>
 
 <style lang="scss">
+#site_header {
+  background-color: #F5E9F3;
+}
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+/* logo */
+.logo {
+  padding-bottom: 1rem;
+}
+}
 </style>
