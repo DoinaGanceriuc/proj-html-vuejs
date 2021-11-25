77743695ab1980949adab8ad2c9c0813d61de981
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-25 17:31:17 +0100

    add product component

---
+++ src/components/Product.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-6" >
+      <img
+        :src="productImage"
+        :alt="productName"
+      />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    productImage: String,
+    productName: String
+  }
+}
+</script>
+
+<style lang="scss">
+.col-6 {
+  & img {
+    width: 100%;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+  }
+}
+</style>
