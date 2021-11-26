f02222f7af5c90f7f2850e637cb2b3aabf2c846a
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-26 11:54:12 +0100

    add FoundedProduct component

---
+++ src/components/FoundedProduct.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="col-3">
+      <img
+        :src="require('../assets/img/' + productImage)"
+        :alt="productName"
+      />
+      <div class="text-center product_text">
+      <h5 class="title">{{productName}}</h5>
+      <p>{{productPrice}}</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    productImage: String,
+    productName: String,
+    productPrice: String
+  }
+}
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+.col-3 {
+  & img {
+    width: 100%;
+    display: block;
+    height: 90%;
+    object-fit: cover;
+  }
+  .product_text {
+    color: $color_title;
+    font-size: 0.9rem
+  }
+}
+</style>
