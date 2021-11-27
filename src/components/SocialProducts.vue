471876d2866165a7a2fa6b7d69970f1ef4f5f641
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-27 17:38:41 +0100

    add social product component

---
+++ src/components/SocialProducts.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row">
+          <div class="col-3">
+            <img src="../assets/img/social-1.jpg" alt="donna chef che usa un coltello per tagliare un prodotto">
+          </div>
+          <div class="col-6 text-center">
+            <div class="stay_touch">
+              <small class="small_text">MADE FOR SHARING</small>
+            <h2 class="title pb-3">Let's stay in touch</h2>
+            <div class="btn">
+              <a href="#">Follow Us</a>
+            </div>
+            </div>
+          </div>
+          <div class="col-3">
+            <img src="../assets/img/social-2.jpg" alt="2 brioches con zucchero aggiunto dall'alto">
+          </div>
+          <div class="col-3">
+            <img src="../assets/img/social-3.jpg" alt="3 panetti di pane tenuti insieme da 2 mani">
+          </div>
+          <div class="col-3">
+            <img src="../assets/img/social-4.jpg" alt="un cesto con diversi tipi di pane">
+          </div>
+          <div class="col-3">
+            <img src="../assets/img/social-5.jpg" alt="una pila di pancake con noci sopra e dello sciroppo che viene versato sopra">
+          </div>
+          <div class="col-3">
+            <img src="../assets/img/social-6.jpg" alt="torta al cioccolato sulla quale viene aggiunto del cacao da uno spargicacao">
+          </div>
+        </div>
+        <!-- /.row -->
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import '../assets/scss/variables.scss';
+  .stay_touch {
+    padding: 4rem;
+    height: 90%;
+    background-color: $bg_color_sharing;
+  }
+</style>
