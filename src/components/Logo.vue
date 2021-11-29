2bee093b0ac62b911dca7c2c49172f9c81474e96
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-11-29 09:23:30 +0100

    create Logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+   <div class="logo">
+        <img src="../assets/img/avada-bakery-logo.png" alt="logo baker purple" width="125"/>
+      </div>
+      <!-- /.logo -->
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+/* logo */
+.logo {
+  padding-bottom: 1rem;
+}
+</style>
